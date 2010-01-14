fd7c6bd932e4a2b492166673786ee106ee64783a
Jeff Coleman <progressions@gmail.com>
2010-01-13 19:32:59 -0600

    Specs

---
+++ lib/serenity.rb
@@ -0,0 +1,61 @@
+require 'yaml'
+# 
+# Returns values from multi-level YAML configuration files.
+# 
+# Gracefully handles error messages when an option is not found.
+#
+# Usage:
+#
+#    Given config.yml:
+#      name: Fred Flintstone
+#      wife:
+#        name: Wilma Flintstone
+#      friend:
+#        name: Barney Rubble
+#        wife: Betty Rubble
+#
+#    @config = Serenity::Configuration.new("config.yml")
+#
+#    @config.get("name")            #=> "Fred Flinstone"
+#    @config.get("wife", "name")    #=> "Wilma Flintstone"
+# 
+module Serenity
+  class Configuration
+    attr_accessor :filename, :options
+  
+    def initialize(filename)
+      @filename = filename
+      @options = YAML.load_file(filename)
+    end
+  
+    def to_hash
+      options
+    end
+  
+    def get(*args)
+      c = options
+    
+      missing_option_index = 0
+    
+      args.each_with_index do |arg, i|
+        if c.is_a?(Hash) && c.has_key?(arg)
+          c = c[arg]
+        else
+          missing_option_index = i
+          raise "Option not found."
+        end
+      end
+    
+      c
+    rescue 
+      $stdout.puts "The following option was not found in #{filename}:"
+      (0..missing_option_index).each do |i|
+        $stdout.puts args[i]
+      end
+      $stdout.puts
+      $stdout.puts "Are you sure #{filename} is up to date?"
+      $stdout.puts
+      raise "Option not found."
+    end
+  end
+end
--- spec/serenity_spec.rb
+++ spec/serenity_spec.rb
@@ -1,7 +1,88 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Serenity" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Configuration" do
+  before(:each) do
+    $stdout.stub!(:puts)
+    @options = {
+      "ship" => {
+        "name" => "Serenity",
+        "class" => "Firefly",
+        "crew" => {
+          "captain" => "Mal Reynolds"
+        }
+      }
+    }
+    @filename = "#config.yml"
+    YAML.stub!(:load_file).with(@filename).and_return(@options)
+  end
+  
+  describe "initialization" do
+    it "should make a new object" do
+      Serenity::Configuration.new(@filename).should_not be_nil
+    end
+    
+    it "should raise an error if file doesn't exist" do
+      YAML.stub!(:load_file).and_raise(Errno::ENOENT)
+      lambda {
+        Serenity::Configuration.new("config_doesnt_exist.yml")
+      }.should raise_error
+    end
+    
+    it "should raise an error if filename is blank" do
+      lambda {
+        Serenity::Configuration.new("")
+      }.should raise_error
+    end
+    
+    it "should raise an error if filename is nil" do
+      lambda {
+        Serenity::Configuration.new
+      }.should raise_error
+    end
+    
+    describe "accessors" do
+      before(:each) do 
+        @config = Serenity::Configuration.new(@filename)
+      end
+      
+      it "should set filename" do
+        @config.filename.should == @filename
+      end
+      
+      it "should return options" do
+        @config.to_hash.should == @options
+      end
+    end
+  end
+  
+  describe "get" do
+    before(:each) do 
+      @config = Serenity::Configuration.new(@filename)
+    end
+    
+    it "should return hash of complex values" do
+      @config.get("ship").should == @options["ship"]
+    end
+    
+    it "should go down several levels" do
+      @config.get("ship", "crew", "captain").should == @options["ship"]["crew"]["captain"]
+    end
+    
+    it "should return error if option doesn't exist" do
+      $stdout.should_receive(:puts).with("The following option was not found in #{@filename}:")
+      lambda {
+        @config.get("ship", "albatross")
+      }.should raise_error
+    end
   end
 end
+
+
+
+
+
+
+
+
+
+
